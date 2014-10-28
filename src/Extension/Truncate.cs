87ae882a715a83482144d87d40990d17c5775601
naile <andreas.caravella@gmail.com>
2014-10-28 15:24:56 +0100

    Truncate string

---
+++ src/Extension/Truncate.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Cacti.Mvc.Web
+{
+    public static class TruncateExtension
+    {
+        public static IHtmlString Truncate(this HtmlHelper helper, string input, int length, string marker = "...")
+        {
+            return input.Length <= length
+                ? MvcHtmlString.Create(input)
+                : MvcHtmlString.Create(input.Substring(0, length) + marker);
+        }
+    }
+}
