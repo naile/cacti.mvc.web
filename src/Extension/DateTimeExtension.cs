546b70b1da42f204b69db5ab2817c007d8224fb5
naile <andreas.caravella@gmail.com>
2014-10-28 12:27:32 +0100

    Added common unix/javascript datetime conversions

---
+++ src/Extension/DateTimeExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Cacti.Mvc.Web
+{
+    public static class DateTimeExtension
+    {
+        public static long ToJavascriptTimestamp(this DateTime dateTime)
+        {
+            return (long)(dateTime - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+        }
+
+        public static long ToUnixTimestamp(this DateTime dateTime)
+        {
+            return (long)(dateTime - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+        }
+    }
+}
