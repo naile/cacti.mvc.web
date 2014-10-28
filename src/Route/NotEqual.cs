97855b2511f97baad7a96779016fd4ef63e28220
naile <andreas.caravella@gmail.com>
2014-10-28 12:50:09 +0100

    NotEqual route constraint

---
+++ src/Route/NotEqual.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Cacti.Mvc.Web
+{
+    public class NotEqual : IRouteConstraint
+    {
+        private readonly string[] _match;
+
+        public NotEqual(string match)
+        {
+            _match = new[] { match };
+        }
+
+        public NotEqual(string[] match)
+        {
+            _match = match;
+        }
+
+        public bool Match(HttpContextBase httpContext,
+            Route route,
+            string parameterName,
+            RouteValueDictionary values,
+            RouteDirection routeDirection)
+        {
+            return _match.All(value => String.Compare(values[parameterName].ToString(),
+                value, StringComparison.OrdinalIgnoreCase) != 0);
+        }
+    }
+}
