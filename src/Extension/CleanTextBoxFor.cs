cfcad1193b8533a3d61246ffe5e3c0c7e9b3c81d
naile <andreas.caravella@gmail.com>
2014-12-13 14:51:14 +0100

    Added CleanTextBoxFor
    Same as TextBoxFor but with spellcheck=false, autocomplete=off and
    autocapitalize=off. Good for username inputs etc.

---
+++ src/Extension/CleanTextBoxFor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace Cacti.Mvc.Web
+{
+    /// <summary>
+    /// Same as TextBoxFor but with spellcheck=false, autocomplete=off and autocapitalize=off
+    /// </summary>
+    public static class CleanTextBoxForExtension
+    {
+        private static IDictionary<string, object> SetHtmlAttributes(object htmlAttributes)
+        {
+            if(htmlAttributes == null)
+                htmlAttributes = new object();
+            var dict = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+
+            dict.Add("spellcheck", "false");
+            dict.Add("autocomplete", "off");
+            dict.Add("autocapitalize", "off");
+
+            return dict;
+        }
+
+        public static MvcHtmlString CleanTextBoxFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression)
+        {
+            return CleanTextBoxFor(htmlHelper, expression, null, null);
+        }
+
+        public static MvcHtmlString CleanTextBoxFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression, string format)
+        {
+            return CleanTextBoxFor(htmlHelper, expression, format, null);
+        }
+
+        public static MvcHtmlString CleanTextBoxFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression, object htmlAttributes)
+        {
+            return CleanTextBoxFor(htmlHelper, expression, null, htmlAttributes);
+        }
+
+        public static MvcHtmlString CleanTextBoxFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TProperty>> expression, string format, object htmlAttributes)
+        {
+            var attributes = SetHtmlAttributes(htmlAttributes);
+
+            return format == null
+                ? System.Web.Mvc.Html.InputExtensions.TextBoxFor(htmlHelper, expression, attributes)
+                : System.Web.Mvc.Html.InputExtensions.TextBoxFor(htmlHelper, expression, format, attributes);
+        }
+    }
+}
---
+++ test/CleanTextBoxFor.cs
@@ -0,0 +1,57 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cacti.Mvc.Web.Test
+{
+    [TestClass]
+    public class CleanTextBoxFor
+    {
+        private HtmlHelper<MyModel> _htmlHelper; 
+        public class MyModel
+        {
+            public string InputField { get; set; }
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _htmlHelper = MvcHelper.GetHtmlHelper<MyModel>(new ViewDataDictionary<MyModel>());
+        }
+
+        [TestMethod]
+        public void InputField_Is_Rendered_With_Correct_Attributes_With_No_Overloads()
+        {
+            var actualHtmlString = _htmlHelper.CleanTextBoxFor(x => x.InputField).ToHtmlString();
+
+            Assert.IsTrue(actualHtmlString.Contains("spellcheck=\"false\""));
+            Assert.IsTrue(actualHtmlString.Contains("autocomplete=\"off\""));
+            Assert.IsTrue(actualHtmlString.Contains("autocapitalize=\"off\""));
+        }
+
+        [TestMethod]
+        public void InputField_Is_Rendered_With_Correct_Attributes_With_Custom_HtmlAttributes()
+        {
+            var actualHtmlString = _htmlHelper.CleanTextBoxFor(
+                x => x.InputField, null, new
+            {
+                @class = "myCustomclass"
+            }).ToHtmlString();
+
+            Assert.IsTrue(actualHtmlString.Contains("spellcheck=\"false\""));
+            Assert.IsTrue(actualHtmlString.Contains("autocomplete=\"off\""));
+            Assert.IsTrue(actualHtmlString.Contains("autocapitalize=\"off\""));
+            Assert.IsTrue(actualHtmlString.Contains("class=\"myCustomclass\""));
+        }
+
+        [TestMethod]
+        public void InputField_Is_Rendered_With_Correct_Attributes_With_Custom_Format()
+        {
+            var actualHtmlString = _htmlHelper.CleanTextBoxFor(
+                x => x.InputField, format: "test").ToHtmlString();
+
+            Assert.IsTrue(actualHtmlString.Contains("spellcheck=\"false\""));
+            Assert.IsTrue(actualHtmlString.Contains("autocomplete=\"off\""));
+            Assert.IsTrue(actualHtmlString.Contains("autocapitalize=\"off\""));
+        }
+    }
+}
