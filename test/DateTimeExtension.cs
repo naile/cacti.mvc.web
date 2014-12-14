111de55f5d67a8675bac58b4c39708f8d265c85a
naile <andreas.caravella@gmail.com>
2014-12-14 12:05:19 +0100

    typo (comment) fix

--- test/DateTimeExtension.cs
+++ test/DateTimeExtension.cs
@@ -9,7 +9,7 @@ namespace Cacti.Mvc.Web.Test
         [TestMethod]
         public void DateTime_ToJavascriptTimestamp_Returns_Correct_Value_For_Non_Null_DateTime()
         {
-            //2014-05-20 14:30:35 = 1369060235020 milliseconds from unix epoch (January 1, 1970 (midnight UTC/GMT))
+            //2013-05-20 14:30:35 = 1369060235020 milliseconds from unix epoch (January 1, 1970 (midnight UTC/GMT))
             var date = new DateTime(2013, 05, 20, 14, 30, 35, 20);
 
             Assert.AreEqual(1369060235020, date.ToJavascriptTimestamp());
@@ -18,7 +18,7 @@ namespace Cacti.Mvc.Web.Test
         [TestMethod]
         public void DateTime_ToUnixTimestamp_Returns_Correct_Value_For_Non_Null_DateTime()
         {
-            //2014-05-20 14:30:35 = 1369060235 seconds from unix epoch (January 1, 1970 (midnight UTC/GMT))
+            //2013-05-20 14:30:35 = 1369060235 seconds from unix epoch (January 1, 1970 (midnight UTC/GMT))
             var date = new DateTime(2013, 05, 20, 14, 30, 35, 20);
 
             Assert.AreEqual(1369060235, date.ToUnixTimestamp());
